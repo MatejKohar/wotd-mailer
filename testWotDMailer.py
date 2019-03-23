e6b1ec477afa497d4ae65ce02ad5d911e0d1388b
Matej Kohar <matej.kohar@gmail.com>
2019-03-23 23:23:58 +0100

    Skeleton test code

---
+++ testWotDMailer.py
@@ -0,0 +1,19 @@
+import email
+import smtplib
+import unittest
+
+import selenium
+
+import WotDMailer
+
+
+class TestWotDMailer(unittest.TestCase):
+
+    def test_scrapeWotD(self):
+        pass
+
+    def test_constructMessage(self):
+        pass
+
+    def test_sendWotDMessage(self):
+        pass
