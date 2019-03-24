839eb54218c6f08fcb03f3a7f1680235de88f50c
Matej Kohar <matej.kohar@gmail.com>
2019-03-24 13:45:21 +0100

    Added test code for constructMessage()

--- testWotDMailer.py
+++ testWotDMailer.py
@@ -7,6 +7,7 @@ import wotd_mailer
 
 class TestWotDMailer(unittest.TestCase):
 
+    @unittest.skip('Testing the message constructor now')
     def test_scrapeWotD(self):
         """Test the scraping function of the WotDMailer script."""
         # Populate correct_answers with the daily Deutsch Perfekt content for
@@ -22,7 +23,20 @@ class TestWotDMailer(unittest.TestCase):
                 self.assertEqual(value, results.get(key))
 
     def test_constructMessage(self):
-        pass
+        scraped_data = {'word': 'die Reiseunterlagen (Pl.)',
+                        'type': 'Nomen',
+                        'explanation': 'Dokumente für eine Reise (z. B. Hotelreservierung, Informationen)',
+                        'example': 'Hast du schon die Reiseunterlagen eingepackt?'
+                         }
+        message = wotd_mailer.constructMessage(scraped_data)
+        self.assertEqual('os.environ.GMAIL_ADDRESS', message['From'])
+        self.assertEqual('os.environ.GMAIL_ADDRESS', message['To'])
+        self.assertEqual('Wort des Tages', message['Subject'])
+        message_body = message.get_content()
+        self.assertIn('Reiseunterlagen', message_body)
+        self.assertIn('Nomen', message_body)
+        self.assertIn('Dokumente', message_body)
+        self.assertIn('eigepackt', message_body)
 
     def test_sendWotDMessage(self):
         pass
